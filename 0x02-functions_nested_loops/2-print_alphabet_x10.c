b07c38416d96eddad8836ef6d6ec673a859ef7ed
Tadele Degefa Gelato <123165752+Alsantade@users.noreply.github.com>
2023-02-21 19:03:40 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,14 +4,13 @@
  *
  *Return: Always 0.
  */
-void print_alphabet_x10(void)
+void print_alphabet(void);
 {
 	int i;
 	char c;
 
-	for (i = 0; i < 10; i++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
-		for (c = 'a'; c <= 'z'; c++)
 			_putchar(c);
 		_putchar('\n');
 											}
