fbb797af1f570553d451b3c32cb24e2620a6d8b8
Tadele Degefa Gelato <123165752+Alsantade@users.noreply.github.com>
2023-02-21 19:11:44 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,11 @@
 #include "main.h"
+
 /**
+*
  * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
+ *
  */
+
 void print_alphabet_x10(void)
 {
 	int count = 0;
@@ -10,7 +14,9 @@ void print_alphabet_x10(void)
 	while (count++ <= 9)
 	{
 		for (c = 'a'; c <= 'z'; c++)
+		{
 			_putchar(c);
+		}
 		_putchar('\n');
 	}
 }
