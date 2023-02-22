3c2df87cec99230ed6f543d5a2e088e00b481f1c
Alsantade <barentutade03@gmail.com>
2023-02-21 18:39:01 -0800

    /** * print_alphabet_x10 - Make alphabet 10 times. * Return: void */
    void print_alpahabet_x10(void)
    {
    	char c;
    	int i = 0;
    
    	while (i <= 9)
    	{
    	for (c = 'a'; c <= 'z'; c++)
    	{
    	_putchar(c);
    	}
    	_putchar('\n');
    	i++;
    	}
    }

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,28 +4,18 @@
  * Return: void
  */
 
-void print_alphabet_x10(void)
+void print_alphabe_x10(void)
 {
         char c;
-        int i = 0;
+	int i = 0;
 
-<<<<<<< HEAD
-	while (i <= 9)
+   	while (i <= 9)
 	{
-	for (c = 'a'; c <= 'z'; c++);
+	for (c = 'a'; c <= 'z'; c++)
+	{
 	_putchar(c);
 	}
 	_putchar('\n');
 	i++;
 	}
-=======
-        while (i <= 9)
-        {
-        for (c = 'a'; c <= 'z'; c++);
-        _putchar(c);
-        }
-        _putchar('\n');
-        i++;
-        }
->>>>>>> fb5e4471bbc358a22ed3da8328f1221b5f0f4182
 }
