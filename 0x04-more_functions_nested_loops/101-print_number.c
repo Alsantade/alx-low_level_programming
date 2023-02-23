00614315eabfcb93ba177294670093fe87df3778
Tadele Degefa Gelato <123165752+Alsantade@users.noreply.github.com>
2023-02-23 21:33:33 +0300

    commit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * print_number - print function
  * @n: input variable
@@ -14,7 +13,7 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		_putchar('_');
+		_putchar('-');
 		m = -n;
 	}
 	else
